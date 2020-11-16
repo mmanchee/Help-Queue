5bf8dd8cb158dea585d3a6a1bbdf0dd79aa198a3
Mike Manchee <mikemanchee@gmail.com>
2020-11-16 10:07:20 -0800

    add Ticket.js and edit App.js to components folder
    Co-authored-by: Vanessa Stewart <vstewart1213@gmail.com>

---
+++ src/components/Ticket.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Ticket() {
+  const name = "Thato";
+  const name2 = "Haley";
+  return (
+    <React.Fragment>
+      <h3>3a</h3>
+      <h3>{name} and {name2}</h3>
+      <p><em>Firebase entries not saving!</em></p>
+      {/* This is a JSX comment. */}
+      <hr />
+    </React.Fragment>
+  );
+}
+
+export default Ticket;
