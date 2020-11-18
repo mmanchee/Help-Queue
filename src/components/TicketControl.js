3d6626a9838f32cbb1c86611b3bf60261c73fa5b
Mike Manchee <mikemanchee@gmail.com>
2020-11-18 14:36:47 -0800

    add part 1 Editing Tickets

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -2,6 +2,7 @@ import React from 'react';
 import NewTicketForm from './NewTicketForm';
 import TicketList from './TicketList';
 import TicketDetail from "./TicketDetail";
+import EditTicketForm from "./EditTicketForm";
 
 class TicketControl extends React.Component {
 
@@ -10,8 +11,13 @@ class TicketControl extends React.Component {
     this.state = {
       formVisibleOnPage: false,
       masterTicketList: [],
-      selectedTicket: null
+      selectedTicket: null,
+      editing: false
     };
+  }
+
+  handleEditClick = () => {
+    this.setState({editing: true});
   }
 
   handleDeletingTicket = (id) => {
@@ -51,9 +57,14 @@ class TicketControl extends React.Component {
   render(){
     let currentlyVisibleState = null;
     let buttonText = null;
-
-    if (this.state.selectedTicket !== null) {
-      currentlyVisibleState = <TicketDetail ticket = {this.state.selectedTicket} onClickDelete = {this.handleDeletingTicket} />
+    if (this.state.editing) {
+      currentlyVisibleState = <EditTicketForm ticket = {this.state.selectedTicket} />
+      buttonText = "Return to Ticket List";
+    } else if (this.state.selectedTicket !== null) {
+      currentlyVisibleState = <TicketDetail 
+        ticket = {this.state.selectedTicket} 
+        onClickDelete = {this.handleDeletingTicket} 
+        onClickingEdit = {this.handleEditClick} />
       buttonText = "Return to Ticket List";
     } else if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />
